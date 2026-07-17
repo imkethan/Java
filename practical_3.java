458e5af1bdf72181715b255b2fa1d43e37e53e30
imkethan <kethanprakashbodduluri@gmail.com>
2026-07-17 09:17:49 +0530

    Update practical_3.java

--- practical_3.java
+++ practical_3.java
@@ -2,5 +2,17 @@ import java.util.Scanner;
 
 public class ScannerExample{
 
-public static void main(string args){
-  
+public static void main(string[] args){
+
+  Scanner sc = new Scanner(System.in);
+
+  System.out.print("Enter your name: ");
+  String name = sc.nextLine();
+
+  if(number >= 0){
+    system.out.println("The number is positive.");
+  } else {
+    System.out.println("The number is Negative.");
+  }
+
+  sc.close();

743c3ff0de4c9758e3e718c8222a41bedb525ae5
imkethan <kethanprakashbodduluri@gmail.com>
2026-07-17 08:53:31 +0530

    Refactor arithmetic and logical operator outputs

---
+++ practical12.java
@@ -0,0 +1,64 @@
+public class Practical2{
+
+  public static void main(string args) {
+
+    int a = 10;
+    int b = 5;
+
+    System.out.println("Arithmetic Operators");
+    System.out.println("Addition = " + (a + b));
+    System.out.println("Subraction = " + (a - b));
+    System.out.println("Multiplication = " + (a * b));
+    System.out.println("Division = " + (a / b));
+
+    System.out.println();
+
+    System.out.println("Relational Operators");
+    System.out.println("a == b : " + (a == b));
+    System.out.println("a != b : " + (a != b));
+    System.out.println("a > b : " + (a > b));
+    System.out.println("a >= b : " + (a >= b));
+    System.out.println("a <= b : " + (a <= b));
+
+    System.out.println();
+
+    boolean x = true;
+    boolean y = false;
+
+    System.out.println("Logical Operators");
+    System.out.println("x && y = " + (x && y));
+    System.out.println("x || y = " + (x || y));
+    System.out.println("!x = " + (!x));
+
+    System.out.println();
+
+    int c = 10;
+
+    System.out.println("Unary Operators");
+    System.out.println("Original Value = " + c);
+    System.out.println("Unary plus = " + +(+c));
+    System.out.println("Unary minus = " + -(+c));
+
+    System.out.println("Pre Increment = " + (++c));
+    System.out.println("Post Increment = " + (c++));
+    System.out.println("After Post Increment = " + c);
+
+    System.out.println("Pre Decrement = " + (--c));
+    System.out.println("Post Decrement = " + (c--));
+    System.out.println("After Post Decrement = " + c);
+
+    System.out.println();
+
+    int p = 5;
+    int q = 3;
+
+    System.out.println("Bitwise Operators");
+    System.out.println("p & q = " + (p & q));
+    System.out.println("p | q = " + (p | q));
+    System.out.println("p ^ q = " + (p ^ q));
+    System.out.println("~p = " + (~p));
+    System.out.println("p << 1 = " + (p << 1));
+    System.out.println("p >> 1 = " + (p >> 1));
+    System.out.println("p >>> 1 = " + (p >>> 1));
+    
+    
